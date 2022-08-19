0e8d86b801b6fde4815e8d1ed158205f5231cfa3
Jannis Morgenstern <nyarthan@proton.me>
2022-08-19 07:18:06 +0200

    refactor: simplify initial impl

--- lua/telescope-code-actions/utils/init.lua
+++ lua/telescope-code-actions/utils/init.lua
@@ -1,44 +1,47 @@
+--[[
+
+links:
+CodeAction interface
+https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#codeAction
+
+--]]
+
 local M = {}
 
-M.map = function(table, cb)
-	local t = {}
-	for k, v in pairs(table) do
-		t[k] = cb(v)
-	end
-	return t
-end
-
-M.normalize_server_actions = function(server_actions)
-	local result = server_actions.result
-
-	if result == nil then
-		vim.notify(server_actions.err.message, vim.log.levels.ERROR)
-		return nil
-	end
-
-	return result
-end
-
-M.get_server_code_actions = function(client)
-	local params = vim.lsp.util.make_range_params()
-	params.context = {
-		diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
-	}
-	local server_actions = client.request_sync("textDocument/codeAction", params)
-	local edits = M.normalize_server_actions(server_actions)
-	return edits
-end
-
 M.get_code_actions = function()
-	local lsp_clients = vim.lsp.buf_get_clients()
-	local titles = {}
-	M.map(lsp_clients, function(client)
-		local edits = M.get_server_code_actions(client)
-		for _, v in pairs(edits) do
-			table.insert(titles, v.title)
+	local clients = vim.lsp.buf_get_clients()
+
+	local actions = {}
+
+	for _, client in pairs(clients) do
+		local params = vim.lsp.util.make_range_params()
+		params.context = {
+			diagnostics = vim.lsp.diagnostic.get_line_diagnostics(),
+		}
+		local response = client.request_sync("textDocument/codeAction", params)
+
+		local result = response.result
+		local error = response.err
+
+		if error then
+			vim.notify(error)
+			return nil
 		end
-	end)
-	return titles
+
+		if result == nil then
+			return nil
+		end
+
+		local titles = vim.tbl_map(function(item)
+			return item.title
+		end, result)
+
+		for _, title in pairs(titles) do
+			table.insert(actions, title)
+		end
+	end
+
+	return actions
 end
 
 return M
