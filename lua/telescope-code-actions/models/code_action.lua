44a0c56886fa18d3c905ae9506e782d6cce65700
Jannis Morgenstern <nyarthan@proton.me>
2022-09-24 22:02:45 +0200

    feat: resolve code actions if necessary

--- lua/telescope-code-actions/models/code_action.lua
+++ lua/telescope-code-actions/models/code_action.lua
@@ -24,18 +24,39 @@ end
 
 function CodeAction:apply()
 	local client = vim.lsp.get_client_by_id(self.meta.client_id)
-	local command = self.server_action.command
-	local edit = self.server_action.edit
-	if self.server_action.command then
-		local params = {
-			command = command.command,
-			arguments = command.arguments,
-			wordDoneToken = command.wordDoneToken,
-		}
-		client.request("workspace/executeCommand", params, nil, self.meta.bufnr)
+
+	local apply = function(resolved_action)
+		local command = resolved_action.command
+		local edit = resolved_action.edit
+
+		if command then
+			local params = {
+				command = command.command,
+				arguments = command.arguments,
+				wordDoneToken = command.wordDoneToken,
+			}
+			client.request("workspace/executeCommand", params, nil, self.meta.bufnr)
+		end
+		if edit then
+			vim.lsp.util.apply_workspace_edit(edit, client.offset_encoding)
+		end
 	end
-	if edit then
-		vim.lsp.util.apply_workspace_edit(edit, client.offset_encoding)
+
+	if
+		not self.server_action.edit
+		and client
+		and vim.tbl_get(client.server_capabilities, "codeActionProvider", "resolveProvider")
+	then
+		client.request("codeAction/resolve", self.server_action, function(err, resolved_action)
+			if err then
+				vim.notify(err.code .. ": " .. err.message, vim.log.levels.ERROR)
+				return
+			end
+
+			apply(resolved_action)
+		end)
+	else
+		apply(self.server_action)
 	end
 end
 
